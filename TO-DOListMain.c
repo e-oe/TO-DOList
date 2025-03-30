16c245fb8967f5b026b42e354b0e2382385f2ee1
e-oe <031890030@ogr.uludag.edu.tr>
2025-03-30 12:16:44 +0300

    Initialized the necessary files

---
+++ TO-DOListMain.c
@@ -0,0 +1,5 @@
+//
+// Created by kurtubi on 3/30/2025.
+//
+
+#include "TO-DOListMain.h"
---
+++ TO-DOListMain.h
@@ -0,0 +1,8 @@
+//
+// Created by kurtubi on 3/30/2025.
+//
+
+#ifndef TO_DOLISTMAIN_H
+#define TO_DOLISTMAIN_H
+
+#endif //TO_DOLISTMAIN_H
