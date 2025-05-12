46b5a93907da6cb8e4a0c8cae1402dabfaa49e6d
e-oe <031890030@ogr.uludag.edu.tr>
2025-05-12 22:27:14 +0300

    Last touches before presentation

--- main.c
+++ main.c
@@ -6,30 +6,4 @@
 
 int main(void) {
 
-    listPtr Listem = CreateList();
-    listPtr Listem2 = CreateList();
-
-
-    AddTask("1",Listem);
-    AddTask("2",Listem);
-    AddTask("3",Listem);
-    DisplayList(Listem);
-    AddTask("4",Listem2);
-    AddTask("5",Listem2);
-    AddTask("6",Listem2);
-    DisplayList(Listem2);
-    Listem=MergeLists(Listem,Listem2);
-    printf("After Merging\n");
-    DisplayList(Listem);
-
-
-
-
-
-
-
-
-
-
-    
 }
